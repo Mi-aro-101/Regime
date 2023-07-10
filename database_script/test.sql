17da321fe7367ff6e6f57711c87925189e0f0227
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2023-07-10 01:32:18 -0400

    login et inscription backend ok + update database

--- database_script/test.sql
+++ database_script/test.sql
@@ -1,3 +1,7 @@
+--database name : Regime
+--username : root
+--password : root
+
 create table if not exists Utilisateur(
     id_utilisateur INTEGER PRIMARY KEY AUTO_INCREMENT,
     nom_utilisateur VARCHAR(21) NOT NULL,
