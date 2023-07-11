6bcc4ec892dffb258a8286133ae650970d43f826
Liantsiky Rasoanaivo <liantsiky@pop-os.localdomain>
2023-07-11 12:29:56 +0300

    "Poids statut model"

---
+++ application/models/Poids_statut_model.php
@@ -0,0 +1,12 @@
+<?php 
+    defined('BASEPATH') OR exit('No direct script access allowed');
+
+    class Poids_statut_model extends CI_Model 
+    {
+        public function get_id_poids_statut($table,$statut_poids){
+            
+            $result = $this->db->get_where($table,['statut_poids' => $statut_poids])->row();
+            return $result[0];
+        }
+    }
+?>
